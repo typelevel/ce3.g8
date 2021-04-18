e37bd4c2be5fb7bb15352bb9af779e382d1ae634
Antonio Gelameris <toniogela89@gmail.com>
2021-04-18 21:33:45 +0200

    Removed optionality for parenthesis since they raise a warning

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -7,5 +7,5 @@ object Main extends IOApp.Simple {
 
   // This is your new "main"!
   def run: IO[Unit] =
-    HelloWorld.say$if(scala3.truthy)$()$endif$.flatMap(IO.println)
+    HelloWorld.say().flatMap(IO.println)
 }
